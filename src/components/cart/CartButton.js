7cb0eb237474d28be89e33f9cc238ec54a9390de
Unknown <chatajazzy@interia.pl>
2018-05-06 15:46:47 +0200

    Add cartbutton component

---
+++ src/components/cart/CartButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartButton = props => {
+  return (
+    <button className="btn btn-default btn-sm" onClick={props.handler}>
+      {props.txt}
+    </button>
+  );
+};
+
+export default CartButton;
