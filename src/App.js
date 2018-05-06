b98897cd93bf9ac9ed63630d24927f28830fcf48
Unknown <chatajazzy@interia.pl>
2018-05-06 15:51:21 +0200

    Add routing to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import AppActions from './actions/app-actions';
+import Catalog from './components/catalog/Catalog';
+import CatalogDetail from './components/product/CatalogDetail';
+import Cart from './components/cart/Cart';
+import Header from './components/header/Header';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>My React App!</h1>
-      </div>
+      <Router>
+        <div className="container">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Catalog} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/item/:item" component={CatalogDetail} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
